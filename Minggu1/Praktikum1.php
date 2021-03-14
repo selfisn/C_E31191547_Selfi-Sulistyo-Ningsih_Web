269138a950368100f5f906bdf2ae03660509e94c
selfisn <selfi.sulistyo2000@gmail.com>
2021-03-14 12:02:45 +0700

    Create Praktikum1.php

---
+++ Minggu1/Praktikum1.php
@@ -0,0 +1,55 @@
+<?php
+// Declare class
+class Car {
+	// properties
+	public $comp;
+	public $color = 'beige';
+	public $husSunRoof = true;
+
+	//method = hello
+	public function hello ()
+	{
+		return "beep";
+	}
+}
+
+//Membuat instance
+$bmw = new Car ();
+$mercedes = new Car ();
+
+//Get values
+$bmw -> color; //beige
+echo "<br />";
+echo $mercedes -> color; //beige
+echo "<hr />";
+
+// Set values
+$bmw->color = 'blue';
+$bmw->comp = "BMW";
+$mercedes->comp = "Mercedes Benz";
+
+// Get values
+echo $bmw->color; // blue
+echo "<br />";
+echo $mercedes->color; // beige
+echo "<br />";
+echo $bmw->comp; // BMW
+echo "<br />";
+echo $mercedes->comp; // Mercedes Benz
+echo "<hr />";
+
+// methods get a beep
+echo $bmw->hello(); // beep
+echo "<br />";
+echo $mercedes->hello(); // beep
+
+/*
+Output :
+beige
+beige
+blue
+beige
+BMW
+Mercedes Benz
+beep
+beep 
