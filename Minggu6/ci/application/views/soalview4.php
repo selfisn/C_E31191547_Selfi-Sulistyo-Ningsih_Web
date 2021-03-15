b4a29ec40d39dd13ce986b90053004d66543434e
selfisn <selfi.sulistyo2000@gmail.com>
2021-03-16 00:08:51 +0700

    Create soalview4.php

---
+++ Minggu6/ci/application/views/soalview4.php
@@ -0,0 +1,9 @@
+<html>
+	<head><title>Controller dan View lebih dari 1 Variabel</title></head>
+	<body>
+		<h2>Mengirim Data dari Controller ke View</h2>
+		<h3><?= $var1 ?></h3>
+		<h3><?= $var2 ?></h3>
+
+	</body>
+</html>
