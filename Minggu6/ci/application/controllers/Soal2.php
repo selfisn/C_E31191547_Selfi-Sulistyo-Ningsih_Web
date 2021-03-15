574efe16f2bc68055c8f11a99899a3ae4df37be9
selfisn <selfi.sulistyo2000@gmail.com>
2021-03-15 23:59:22 +0700

    Create Soal2.php

---
+++ Minggu6/ci/application/controllers/Soal2.php
@@ -0,0 +1,10 @@
+<?php
+	class Soal2 extends CI_Controller{
+		public function index() {
+			echo '<h4>Soal/Tugas Nomor 2 Minggu 6</h4>';
+			echo 'Nama : Riska Virliana M H' . '<br />';
+			echo 'NIM : E31192024' . '<br />';
+			echo 'Golongan : C';
+		}
+	}
+?>
