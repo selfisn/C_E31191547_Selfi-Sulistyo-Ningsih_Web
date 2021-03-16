a6f6272d84b4625c756d7da8a89e6b21b9a1097e
selfisn <selfi.sulistyo2000@gmail.com>
2021-03-16 09:30:32 +0700

    Create Soal2.php

---
+++ Minggu6/ci/application/controllers/Soal2.php
@@ -0,0 +1,10 @@
+<?php
+	class Soal2 extends CI_Controller{
+		public function index() {
+			echo '<h4>Soal/Tugas Nomor 2 Minggu 6</h4>';
+			echo 'Nama : Selfi Sulistyo Ningsih' . '<br />';
+			echo 'NIM : E31191547' . '<br />';
+			echo 'Golongan : C';
+		}
+	}
+?>
