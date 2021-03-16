8664b980cc8854a643906714884a1a310228cef1
selfisn <selfi.sulistyo2000@gmail.com>
2021-03-16 09:00:25 +0700

    Update Soal2.php

--- Minggu6/ci/application/controllers/Soal2.php
+++ Minggu6/ci/application/controllers/Soal2.php
@@ -2,8 +2,8 @@
 	class Soal2 extends CI_Controller{
 		public function index() {
 			echo '<h4>Soal/Tugas Nomor 2 Minggu 6</h4>';
-			echo 'Nama : Riska Virliana M H' . '<br />';
-			echo 'NIM : E31192024' . '<br />';
+			echo 'Nama : Selfi Sulistyo Ningsih' . '<br />';
+			echo 'NIM : E31191547' . '<br />';
 			echo 'Golongan : C';
 		}
 	}
